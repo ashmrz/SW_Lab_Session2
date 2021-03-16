7f299130ea9620c512c2e8055b9a1d9deabf69a1
AradM <arad.mohammadi99@gmail.com>
2021-03-16 11:50:20 +0330

    add Sqaure Class with getter and setter`

---
+++ Geometry/src/Square.java
@@ -0,0 +1,25 @@
+public class Square {
+	private double width;
+
+	public Square(double width) throws Exception {
+		if (width < 0){
+			throw new Exception("Negative Width");
+		}	
+		this.width = width;
+	}
+	
+	public double getWidth() {
+		return width;
+	}
+	
+	public void setWidth(double width) throws Exception {
+		if (width < 0){
+			throw new Exception("Negative Width");
+		}
+		this.width = width;
+	}
+	
+	public double computeArea() {
+		return width * width;
+	}
+}
