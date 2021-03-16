f8c1309e6516d076f30b1debc80f8cc43afaf1e6
ashmrz <ashkan.m10@gmail.com>
2021-03-16 11:40:19 +0330

    Minor bug fixes

--- Geometry/src/Rectangle.java
+++ Geometry/src/Rectangle.java
@@ -1,38 +1,38 @@
 public class Rectangle {
-	private double width;
-	private double height;
+    private double width;
+    private double height;
 
-	public Rectangle(double width, double height) throws Exception {
-		if (width < 0 || height < 0) {
-			throw new Exception("Neagtive Width or Height");
-		}
-		this.width = width;
-		this.height = height;
-	}
+    public Rectangle(double width, double height) throws Exception {
+        if (width < 0 || height < 0) {
+            throw new Exception("Neagtive Width or Height");
+        }
+        this.width = width;
+        this.height = height;
+    }
 	
 	public void setHeight(double height) throws Exception{
 		if (height < 0){
-			throw new Exepction("Neagtive Height");
+			throw new Exception("Neagtive Height");
 		}
 		this.height = height;
 	}
 	
-	public getHeight(){
+	public double getHeight(){
 		return height;
 	}
 	
 	public void setWidth(double width) throws Exception{
 		if (width < 0){
-			throw new Exepction("Neagtive Width");
+			throw new Exception("Neagtive Width");
 		}
 		this.width = width;
 	}
 	
-	public getWidth(){
+	public double getWidth(){
 		return width;
 	}
 
-	public double computeArea() {
-		return width * height;
-	}
+    public double computeArea() {
+        return width * height;
+    }
 }
